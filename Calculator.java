2a6456b4245dcca1a16a9745c184bf5ab8b4cd44
Nayana Harish <harish0878@saskpolytech.ca>
2023-11-19 20:18:07 -0600

    Calculator with while loop to repeat the operations

--- Calculator.java
+++ Calculator.java
@@ -3,44 +3,63 @@ package SwitchStatement;
 import java.util.Scanner;
 
 public class Calculator {
-
     public static void main(String[] args) {
 
-        System.out.println("Enter the oeration" +
-                "1.Add 2.Sub 3. Mul 4.Div");
-        Scanner scanner = new Scanner(System.in);
-        int op = scanner.nextInt();
-
-        System.out.println("Enter the first operand:");
-        int num1 = scanner.nextInt();
-        System.out.println("Enter the second operand:");
-
-        int num2 = scanner.nextInt();
-
-       double r =  Calculator(num1,num2,op);
-        System.out.println("Result is "+r);
-
-
-    }
-
-    public static double Calculator(int num1,int num2,int operator)
-    {
-        int result = 0 ;
-        switch (operator)
-        {
-            case 1 :
-                result = num1 + num2;
-                break;
-            case 2 :
-                result = num1 - num2;
-                break;
-            case 3 :
-                result = num1 * num2;
-                break;
-            case 4 :
-                result = num1 / num2;
-                break;
+        char repeat = 'y';
+        while(repeat=='y') {
+            System.out.println("Enter the first operand ");
+            Scanner scanner = new Scanner(System.in);
+            double op1 = scanner.nextInt();
+            System.out.println("Enter the second operand ");
+            double op2 = scanner.nextInt();
+            System.out.println("Enter the operation to be performed:");
+            System.out.println("1. Add\n2. Subtraction\n3 .Multiplication\n4 .Division");
+            int operand = scanner.nextInt();
+            String result = calculationProcess(operand, op1, op2);
+            System.out.println("Result:\n " + result);
+            System.out.println("Do you want to continue please press y otherwise enter n");
+            repeat = scanner.next().charAt(0);
         }
-return result;
+
     }
+
+    public static String calculationProcess(int operand , double op1 , double op2 )
+    {
+        char op = ' ';
+        double  result = 0.0;
+        switch (operand)
+        {
+            case 1 : op = '+';
+                result = add(op1, op2) ;
+                break ;
+            case 2 : op = '-';
+                result = sub(op1,op2) ;
+                break ;
+            case 3 : op = '*';
+                result = mul(op1,op2);
+                break ;
+            case 4 : op = '/';
+                result = div(op1,op2) ;
+                break ;
+
+        }
+        return op1+" "+op+" "+op2 + " = "+result ;
+    }
+    public static double add(double op1 ,double op2)
+    {
+        return op1 + op2;
+    }
+    public static double sub(double op1 ,double op2)
+    {
+        return op1 - op2;
+    }
+    public static double mul(double op1 ,double op2)
+    {
+        return op1 * op2;
+    }
+    public static double div(double op1 ,double op2)
+    {
+        return op1 / op2;
+    }
+
 }
